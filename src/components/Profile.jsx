077159529639cfc1fa5a897acc1ca82e7552ca55
Dinobondhu-shill <dinu.webdev@gmail.com>
2025-01-14 18:26:10 +0600

    profile menu bar reference added

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,18 +1,39 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect, useRef } from 'react';
 import { PiCallBellThin  } from "react-icons/pi";
 import { FaRegEdit } from "react-icons/fa";
 import { IoChatbubblesOutline, IoKeyOutline  } from "react-icons/io5";
 import { CiLocationOn, CiLogout  } from "react-icons/ci";
 import { Link } from 'react-router-dom';
 import { BlackContext } from '@/Context/Context';
+import { IoIosCloseCircleOutline } from 'react-icons/io';
 
 const Profile = ({data}) => {
-  const {  showProfile } = useContext(BlackContext);
+  const {  showProfile, setShowProfile } = useContext(BlackContext);
+  const profileRef = useRef(null);
 
+    // Function to handle click outside the sidebar
+    const handleClickOutside = (event) => {
+      if (profileRef.current && !profileRef.current.contains(event.target)) {
+        setShowProfile(false);
+      }
+    };
+
+    useEffect(() => {
+      // Add event listener for clicks
+      document.addEventListener("mousedown", handleClickOutside);
+  
+      // Cleanup event listener on component unmount
+      return () => {
+        document.removeEventListener("mousedown", handleClickOutside);
+      };
+    }, []);
   return (
-    <div className={`fixed top-16 right-4 w-full max-w-80 bg-white shadow-lg p-6 transition-transform duration-1000 ease-in-out ${
+    <div
+    ref={profileRef}
+     className={`fixed top-16 right-4 rounded-sm w-full max-w-80 bg-white shadow-2xl p-6 transition-transform duration-1000 ease-in-out ${
     showProfile ? "translate-x-0" : "translate-x-full"
     }`}>
+     
       <div className='flex gap-5 items-center'> 
         <img src={data?.photo} alt="" className='w-16 h-16 object-cover rounded-full ' />
         <div>
@@ -20,6 +41,8 @@ const Profile = ({data}) => {
           <p className='text-sm text-gray-500'>{data?.email}</p>
           <p> ৳ 0.00</p>
         </div>
+        <span onClick={()=>setShowProfile(!showProfile)}
+         id="cross" className="relative -top-10 -right-6 text-3xl cursor-pointer text-pink-600 z-50"><IoIosCloseCircleOutline /></span>
       </div>
       {/* link list of profile  */}
       <div className='mt-10'>
