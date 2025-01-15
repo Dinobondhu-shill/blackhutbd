597d07531c56dc92b029d1edfe4a6f535217a9f9
Dinobondhu-shill <dinu.webdev@gmail.com>
2025-01-15 14:39:58 +0600

    added locatioon adding page

--- src/Pages/Profile/Address.jsx
+++ src/Pages/Profile/Address.jsx
@@ -1,9 +1,49 @@
-import React from 'react';
-
+import { Button } from "@/components/ui/button"
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
 const Address = () => {
   return (
-    <div className="lg:py-10 relative top-12">
-      Address
+    <div className="lg:py-10 relative top-8">
+      <div className='my-5 flex justify-around'>
+      <h3 className=' font-bold text-3xl mb-5'>Address</h3>
+      <Dialog>
+      <DialogTrigger asChild>
+        <Button variant="outline" className='bg-[#e440a536] rounded-full hover:bg-pink-600 hover:text-white text-pink-600 group'> <span className="bg-pink-600 w-[20px] h-[20px] rounded-full group-hover:bg-white group-hover:text-pink-600 text-white font-bold">+</span>Add New</Button>
+      </DialogTrigger>
+      <DialogContent className="sm:max-w-[425px]">
+        <DialogHeader>
+          <DialogTitle>Your Address</DialogTitle>
+        </DialogHeader>
+        <div className="grid gap-4 py-4">
+          <div className="grid grid-cols-4 items-center gap-4">
+            <Label htmlFor="name" className="text-right">
+              Name
+            </Label>
+            <Input id="name" value="Pedro Duarte" className="col-span-3" />
+          </div>
+          <div className="grid grid-cols-4 items-center gap-4">
+            <Label htmlFor="username" className="text-right">
+              Username
+            </Label>
+            <Input id="username" value="@peduarte" className="col-span-3" />
+          </div>
+        </div>
+        <DialogFooter>
+          <Button type="submit">Save changes</Button>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
+
+      </div>
     </div>
   );
 };
